1b05094058355ff38ef08eed61af4cb188c61441
BerissaMuyizere <muyizereberissa@gmail.com>
2025-05-20 15:41:21 +0300

    learn language

--- question1/index.js
+++ question1/index.js
@@ -1,40 +1,45 @@
-// class Languages {
-//     constructor(name, vocabulary, quizes, exercises, answers, resources) {
-//         this.name = name;
-//         this.vocabulary = vocabulary;
-//         this.quizes = quizes;
-//         this.exercises = exercises;
-//         this.answers = answers;
-//         this.resources = resources
-//     }
-// };
+class Languages {
+    constructor(language) {
+        this.language = language;
+    }
+};
 
-// Languages.prototype.availableLanguage = function (targetLanguage, proficiencylevel) {
-//     for (lang in this.language) {
-//         if (lang === targetLanguage && this.language.proficiency) {
-
-//         }
-//     }
-
-// }
-
-const learnLanguage = ( targetLanguage, proficiencylevel) =>{
-
-const allLanguages = [{
-    name: "French", proficiency: ["advanced", "intermediate", "beginner"],
-    materials: [{ level: "Beginner", quiz: ["question1", "question2", "question3"], exercises: ["exercise1", "exercises2", "exercises3"], answers: ["okay", "by", "hello"], vocabulary: ["verbs", "proverbes", "nouns"] }, 
-    { level: "Intermediate", quiz: ["question1", "question2", "question3"] , exercises: ["exercise1", "exercises2", "exercises3"], answers: ["okay", "by", "hello"], vocabulary: ["verbs", "proverbes", "nouns"]},
-     { level: "advanced", quiz: ["question1", "question2", "question3"], exercises: ["exercise1", "exercises2", "exercises3"], answers: ["okay", "by", "hello"], vocabulary: ["verbs", "proverbes", "nouns"] }],
-}, {
-    name: "English", proficiency: ["advanced", "intermediate", "beginner"],
-    materials: [{ level: "Beginner", quiz: ["question1", "question2", "question3"], exercises: ["exercise1", "exercises2", "exercises3"], answers: ["okay", "by", "hello"], vocabulary: ["verbs", "proverbes", "nouns"] }, 
-    { level: "Intermediate", quiz: ["question1", "question2", "question3"] , exercises: ["exercise1", "exercises2", "exercises3"], answers: ["okay", "by", "hello"], vocabulary: ["verbs", "proverbes", "nouns"]},
-     { level: "advanced", quiz: ["question1", "question2", "question3"], exercises: ["exercise1", "exercises2", "exercises3"], answers: ["okay", "by", "hello"], vocabulary: ["verbs", "proverbes", "nouns"] }],
-}]
-
-    for (language in allLanguages){
-        if(language.name === targetLanguage  && language.proficiency === proficiencylevel){
-            
+Languages.prototype.availableLanguage = function (targetLanguage, proficiencylevel) {
+    for (lang in this.language) {
+        if (lang.name === targetLanguage ) {
+            return lang.exercises, lang.materials 
         }
+        else console.log("language not found")
     }
 }
+
+Languages.prototype.complete = function(answers){
+    
+
+}
+ const allLanguages = [{
+        name: "French", proficiency: ["advanced", "intermediate", "beginner"],
+        materials: [{ level: "Beginner", quiz: ["question1", "question2", "question3"], exercises: ["exercise1", "exercises2", "exercises3"], answers: ["okay", "by", "hello"], vocabulary: ["verbs", "proverbes", "nouns"] },
+        { level: "Intermediate", quiz: ["question1", "question2", "question3"], exercises: ["exercise1", "exercises2", "exercises3"], answers: ["okay", "by", "hello"], vocabulary: ["verbs", "proverbes", "nouns"] },
+        { level: "advanced", quiz: ["question1", "question2", "question3"], exercises: ["exercise1", "exercises2", "exercises3"], answers: ["okay", "by", "hello"], vocabulary: ["verbs", "proverbes", "nouns"] }],
+    }, {
+        name: "English", proficiency: ["advanced", "intermediate", "beginner"],
+        materials: [{ level: "Beginner", quiz: ["question1", "question2", "question3"], exercises: ["exercise1", "exercises2", "exercises3"], answers: ["okay", "by", "hello"], vocabulary: ["verbs", "proverbes", "nouns"] },
+        { level: "Intermediate", quiz: ["question1", "question2", "question3"], exercises: ["exercise1", "exercises2", "exercises3"], answers: ["okay", "by", "hello"], vocabulary: ["verbs", "proverbes", "nouns"] },
+        { level: "advanced", quiz: ["question1", "question2", "question3"], exercises: ["exercise1", "exercises2", "exercises3"], answers: ["okay", "by", "hello"], vocabulary: ["verbs", "proverbes", "nouns"] }],
+    }]
+
+    const languages = new Languages(allLanguages);
+    console.log(languages);
+    languages.availableLanguage()
+
+// const learnLanguage = (targetLanguage, proficiencylevel) => {
+
+//     for (language in allLanguages) {
+//         if (language.name === targetLanguage && language.materials.level === proficiencylevel) {
+//             return language.exercises, language.quiz, language.vocabulary
+//         }
+//         else console.log("language not found")
+//     }
+// }
+// learnLanguage("French", "Beginner")
